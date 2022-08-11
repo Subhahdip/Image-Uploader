249c6eeb459563cfe6070bbbdfc67774d3330980
Subhadip Sarkar <edusubhadip@gmail.com>
2022-08-11 13:01:37 +0000

    Initial stage

---
+++ src/components/imageCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function Card() {
+  return (
+    <div className = "outer-card">
+      <h4>Upload your Image </h4>
+      <p>File should be in JPEG, PNG, etc.</p>
+    </div>
+  );
+}
